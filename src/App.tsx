e87f159934ad116c1069563c73d2d2c2b0346160
Ive Tourinho <ivetourinho@Ives-MacBook-Pro.local>
2024-06-14 12:09:51 -0300

    feat: basic react-hooks-form and zod

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import './App.css'
+
+const schema = z.object({
+  username: z.string().min(2).regex(/^[A-Za-z]+$/i, "Only letters are allowed"),
+  area: z.string(),
+  password: z.string(),
+})
+.refine((data) => console.log('data', data))
+
+type FormData = z.infer<typeof schema>
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  })
+  const onSubmit = (data: any) => {
+
+  }
+
+  console.log('errors', errors);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("username")} />
+      <p>{errors.username?.message?.toString()}</p>
+
+      <input {...register("password")} />
+      <p>{errors.password?.message?.toString()}</p>
+
+
+      <select {...register("area")}>
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </select>
+      <p>{errors.area?.message?.toString()}</p>
+      
+      <input type="submit" />
+    </form>
+  )
+}
+
+export default App
